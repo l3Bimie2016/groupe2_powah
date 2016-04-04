f1ed9933c4cd70e66a000f3c9af3e461d693412a
Maxime Riaux <maxime.riaux@gmail.com>
2016-04-04 17:07:11 +0200

    token

--- vertx_assurance/src/main/java/fr/assurance/App.java
+++ vertx_assurance/src/main/java/fr/assurance/App.java
@@ -2,6 +2,9 @@ package fr.assurance;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.jwt.JWTAuth;
+import io.vertx.ext.auth.jwt.JWTOptions;
 import io.vertx.ext.web.Router;
 
 /**
@@ -18,12 +21,25 @@ public class App extends AbstractVerticle {
     @Override
     public void start() throws Exception {
         Router router = Router.router(vertx);
-        router.get("/verts/index").handler(x -> {
-            x.response().end("Hello from vertx !");
-        });
 
         vertx.createHttpServer()
                 .requestHandler(router::accept)
                 .listen(8090);
+
+        JsonObject config = new JsonObject().put("keyStore", new JsonObject()
+                .put("path", "keystore.jceks")
+                .put("type", "jceks")
+                .put("password", "secret"));
+
+        JWTAuth provider = JWTAuth.create(vertx, config);
+
+        router.get("/login").handler(x -> {
+            String token = "";
+            // "paulo".equals(username) && "super_secret".equals(password)
+            if (true) {
+                token = provider.generateToken(new JsonObject().put("sub", "paulo"), new JWTOptions());
+            }
+            x.response().end("Hello from vertx : "+token);
+        });
     }
 }
