7c01ac51dca4e618ce4f1a81040b1e11ca682d41
Maxime Riaux <maxime.riaux@gmail.com>
2016-04-05 13:33:00 +0200

    add session ApplicationData 2ème

---
+++ spring_assurance/src/main/java/fr/assurance/bean/ApplicationData.java
@@ -0,0 +1,22 @@
+package fr.assurance.bean;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Max on 05/04/2016.
+ */
+@Component
+@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS, scopeName = "session")
+public class ApplicationData {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ spring_assurance/src/main/java/fr/assurance/controller/IndexController.java
@@ -0,0 +1,17 @@
+package fr.assurance.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class IndexController {
+
+	@RequestMapping(path = "/",method = RequestMethod.GET)
+	public String indexView() {
+		return "Cououc";
+	}
+}
