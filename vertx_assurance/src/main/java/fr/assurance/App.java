04d5d186e77926506a5d0136b61d37eca762c7a1
Maxime Riaux <maxime.riaux@gmail.com>
2016-04-06 14:11:02 +0200

    add vertx mysql bdd connection

--- vertx_assurance/src/main/java/fr/assurance/App.java
+++ vertx_assurance/src/main/java/fr/assurance/App.java
@@ -2,12 +2,19 @@ package fr.assurance;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.asyncsql.AsyncSQLClient;
+import io.vertx.ext.asyncsql.MySQLClient;
 import io.vertx.ext.auth.jwt.JWTAuth;
 import io.vertx.ext.auth.jwt.JWTOptions;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.SQLConnection;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.JWTAuthHandler;
+
+import java.util.List;
 
 /**
  * Created by Max on 04/04/2016.
@@ -51,5 +58,50 @@ public class App extends AbstractVerticle {
 
             x.response().end("{success:'"+success+"', token:'"+token+"'}");
         });
+
+        router.get("/list").handler(x -> { System.out.println("-- /list");
+
+            JsonObject mySQLClientConfig = new JsonObject()
+                    .put("host", "localhost")
+                    .put("port", 3306)
+                    .put("username", "rootpass")
+                    .put("password", "plop")
+                    .put("database", "vertx_assurance");
+            AsyncSQLClient mySQLClient = MySQLClient.createShared(vertx, mySQLClientConfig);
+
+            mySQLClient.getConnection(res -> { System.out.println("-- 1");
+
+                if (res.succeeded()) { System.out.println("-- 2");
+
+                    SQLConnection connection = res.result();
+
+
+                    JsonObject json = new JsonObject();
+
+                    connection.query("SELECT * FROM voiture", query -> {
+                        System.out.println("-- 3");
+                        if (query.succeeded()) {
+                            System.out.println("-- 4");
+                            // Get the result set
+                            ResultSet resultSet = query.result();
+                            List<JsonArray> results = resultSet.getResults();
+                            int i = 1;
+                            for (JsonArray row : results) {
+                                json.put("row"+i, row.getInteger(0) + "," + row.getInteger(1) + "," + row.getInteger(2) + "," + row.getInteger(3));
+                                i++;
+                            }
+                        } else {
+                            System.out.println("-- 5");
+                            // Failed!
+                        }
+                        x.response().end(json.toString());
+                    });
+                } else { System.out.println("-- 6 "+res.cause().getMessage());
+                    // Failed to get connection - deal with it
+                }
+            });
+        });
+
     }
+
 }
