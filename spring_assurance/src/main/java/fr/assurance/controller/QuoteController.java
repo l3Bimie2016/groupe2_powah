a1b6ece6bacdba751a1d86e0105b251c8868e06d
Lucien Bramard <maxime.santier@imie-rennes.fr>
2016-05-11 14:25:21 +0200

    Mise au propre avec QuoteService

--- spring_assurance/src/main/java/fr/assurance/controller/QuoteController.java
+++ spring_assurance/src/main/java/fr/assurance/controller/QuoteController.java
@@ -14,14 +14,14 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
-import fr.assurance.dao.QuoteRepository;
 import fr.assurance.entities.Quote;
+import fr.assurance.service.QuoteService;
 
 @Controller
 @SessionAttributes("quote")
 public class QuoteController {
 	@Autowired
-	private QuoteRepository quoteManager;
+	private QuoteService quoteManager;
 	
 	@RequestMapping(path="/quote", method=RequestMethod.GET)
 	public String indexView() {
@@ -42,8 +42,17 @@ public class QuoteController {
 	public ModelAndView quoteResume(@RequestParam("idQuote") Integer idQuote, HttpServletRequest request) {
 		Quote quote = quoteManager.findOne(idQuote);
 		request.getSession().setAttribute("quote", quote);
-		System.out.println("QuoteId: " + quote.getId());
-		System.out.println("QuoteStep: " + quote.getStep());
-		return new ModelAndView("redirect:/quote/vehicule.form?_page=" + quote.getStep(), "quote", quote);
+		String url = null;
+		
+		
+		if (quote.getType().equals("VehiculeQuote")) {
+			url = "/quote/vehicule.form?_page=";
+		}
+		
+		if (quote.getType().equals("HouseQuote")) {
+			url = "/quote/home.form?_page=";
+		}
+		
+		return new ModelAndView("redirect:" + url + quote.getStep(), "quote", quote);
 	}
 }
--- spring_assurance/src/main/java/fr/assurance/service/QuoteService.java
+++ spring_assurance/src/main/java/fr/assurance/service/QuoteService.java
@@ -2,7 +2,11 @@ package fr.assurance.service;
 
 import fr.assurance.dao.QuoteRepository;
 import fr.assurance.entities.HouseQuote;
+import fr.assurance.entities.Quote;
 import fr.assurance.entities.VehiculeQuote;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,4 +30,12 @@ public class QuoteService {
     public void delete(Integer id) {
         quoteRepo.delete(id);
     }
+
+	public List<Quote> findByDone(boolean done) {
+		return quoteRepo.findByDone(done);
+	}
+
+	public Quote findOne(Integer idQuote) {
+		return quoteRepo.findOne(idQuote);
+	}
 }
